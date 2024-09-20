950bf9fb3d7268ea2b2b451013d838d45f768d34
Brockton Heaton <Bheaton1016@gmail.com>
2024-09-20 12:24:51 -0400

    Fixxed issues with Library. Also re organized the calculator to correctly match the disign i had in mind

--- Fullsail calculator/CalculatorUI.cpp
+++ Fullsail calculator/CalculatorUI.cpp
@@ -9,38 +9,38 @@ CalculatorUI::CalculatorUI() : wxFrame(nullptr, wxID_ANY, "Calculato", wxDefault
 	mainSizer->Add(m_screen, 0, wxEXPAND | wxALL, 10);
 	
 	//Grid
-	wxGridSizer* grid = new wxGridSizer(5, 4, 5, 5);
+	wxGridSizer* grid = new wxGridSizer(5, 5, 5, 5);
 
-	//Num Buttons
-	grid->Add(new wxButton(this, 1000, "1"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1001, "2"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1002, "3"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1003, "4"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1004, "5"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1005, "6"), 0, wxEXPAND);
+	//First row
 	grid->Add(new wxButton(this, 1006, "7"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1007, "8"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1008, "9"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1009, "0"), 0, wxEXPAND);
-	
-	//Math Operations
 	grid->Add(new wxButton(this, 1100, "+"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1101, "-"), 0, wxEXPAND);
+	//Second Row
+	grid->Add(new wxButton(this, 1003, "4"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1004, "5"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1005, "6"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1102, "*"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1103, "/"), 0, wxEXPAND);
+	//Third Row
+	grid->Add(new wxButton(this, 1000, "1"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1001, "2"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1002, "3"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1104, "%"), 0, wxEXPAND);
-
-	//Trig Operations
+	grid->Add(new wxButton(this, 1302, "Back"), 0, wxEXPAND);
+	//Forth Row
+	grid->Add(new wxButton(this, 1304, "+/-"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1009, "0"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1303, "Dec"), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1300, "="), 0, wxEXPAND);
+	grid->Add(new wxButton(this, 1301, "Clear"), 0, wxEXPAND);
+	
+	//Fith row
 	grid->Add(new wxButton(this, 1200, "sin"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1201, "cos"), 0, wxEXPAND);
 	grid->Add(new wxButton(this, 1202, "tan"), 0, wxEXPAND);
 
-	//Other Functions
-	grid->Add(new wxButton(this, 1300, "="), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1301, "Clear"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1302, "Back"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1303, "Dec"), 0, wxEXPAND);
-	grid->Add(new wxButton(this, 1304, "+/-"), 0, wxEXPAND);
 
 	mainSizer->Add(grid, 1, wxEXPAND | wxALL, 10);
 	SetSizer(mainSizer);
